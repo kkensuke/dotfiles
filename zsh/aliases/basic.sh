c56a680528dab08da104d30c35e906df9b136949
kkensuke <n4659017382@gmail.com>
2023-08-16 18:28:03 +0900

    🔧 MAINTAIN:

--- zsh/aliases/basic.sh
+++ zsh/aliases/basic.sh
@@ -9,7 +9,6 @@ alias ds='du -d 1 -h 2>/dev/null | sort -h'
 alias pwd='sed "s/ /\\\ /g" <<< ${PWD/#$HOME/"~"}'
 alias p='pwd'
 alias path='echo -e ${PATH//:/\\n}'
-
 tre() { tree -ahC -L "$2" -I '.git|venv|.DS_Store' --dirsfirst "$1"}
 
 ## change directory ##
@@ -36,7 +35,7 @@ alias rm='rm -iv'
 alias rf='rm -rf'
 alias v='vi'
 
-## global aliases
+## global aliases ##
 alias -g G='| grep'
 alias -g H='| head'
 alias -g T='| tail'
@@ -45,17 +44,13 @@ alias -g NE='2>/dev/null'
 alias -g NO='&>/dev/null'
 
 ## search ##
+alias grep='grep --color'
 fb() { find . -size +$2M -type f -name $1 -exec ls -lhS "{}" \; | awk '{print $5,$9}' }
 fd() { find . -name "*.$1" -type f -delete }
 rn() { for filename in *.$1; do mv -f "$filename" $(echo "$filename" | sed -e "s/$2//g"); done }
 dif(){ diff --color -u $1 $2 }
-alias imgopt='open -a ImageOptim .'
-alias grep='grep --color'
 
-## others ##
-alias pb='pbcopy'
-alias his='history'
-alias rl='exec ${SHELL} -l' #reload
+## make ##
 tc() { touch $1 && c $1 }
 mkc() { mkdir $1 && cd $1}
 
@@ -69,3 +64,11 @@ alias cpvj='cp -fv ~/Library/Application\ Support/Code/User/settings.json ~/My\ 
 			cp -fv ~/.vscode/extensions/extensions.json ~/My\ Drive/app/github/dotfiles/vscode/extensions.json'
 alias opjb='gj; open /Applications/Firefox.app _build/html/index.html'
 alias fire='open /Applications/Firefox.app'
+
+## others ##
+alias pb='pbcopy'
+alias his='history'
+alias rl='exec ${SHELL} -l' #reload
+alias imgopt='open -a ImageOptim .'
+alias -s py=python
+alias -s html=fire
