171aa495aad684a1d5ece7bd937ccf631ae160f0
kkensuke <n4659017382@gmail.com>
2025-09-16 06:04:29 +0900

    🔧 MAINTAIN:

--- zsh/aliases/encrypt.sh
+++ zsh/aliases/encrypt.sh
@@ -1,10 +1,14 @@
-zipen(){
-    zip -er enc.zip "$@"
+zipen() {
+  if [ $# -lt 1 ]; then
+    echo "Usage: zipen <file-or-folder>"
+    return 1
+  fi
+
+  local target="$1"
+  local base="$(basename "$target")"
+  zip -er "enc_${base}.zip" "$target"
 }
 
-zipdec(){
-    unzip -d dec enc.zip "$@"
-}
 
 
 # --- OpenSSL Encryption/Decryption Functions (macOS Version) ---
