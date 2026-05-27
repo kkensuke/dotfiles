80923c6543e70b12f2c2e1c47171b54d610e75f5
kkensuke <47603428+kkensuke@users.noreply.github.com>
2026-05-27 11:57:37 +0900

    🔧 MAINTAIN:

--- zsh/aliases/mac.sh
+++ zsh/aliases/mac.sh
@@ -18,3 +18,19 @@ alias nohidden='chflags nohidden'
 # sleep setting
 alias sleepon='sudo pmset -a disablesleep 0'
 alias sleepoff='sudo pmset -a disablesleep 1'
+
+# Switch audio output
+function _toggle_audio_source() {
+  # Get the current audio output device
+  local current=$(SwitchAudioSource -c)
+  
+  if [ "$current" = "MacBook Air Speakers" ]; then
+    SwitchAudioSource -s "Soundcore 3" >/dev/null
+    echo "\n🎧 Output switched to: Soundcore 3"
+  else
+    # If it's Soundcore 3 or any other device, switch back to MacBook
+    SwitchAudioSource -s "MacBook Air Speakers" >/dev/null
+    echo "\n💻 Output switched to: MacBook Air Speakers"
+  fi
+}
+alias ts="_toggle_audio_source"
