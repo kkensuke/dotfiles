0dc8332ef38891689a44b94205ad4e23b1ebafd2
kkensuke <47603428+kkensuke@users.noreply.github.com>
2026-08-31 02:52:05 +0900

    🔧 MAINTAIN:

--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,5 +1,2 @@
 # Add Homebrew to PATH
 eval "$(/opt/homebrew/bin/brew shellenv)"
-
-# zoxide
-eval "$(zoxide init zsh)"
--- zsh/settings/hooks.zsh
+++ zsh/settings/hooks.zsh
@@ -2,7 +2,7 @@ autoload -Uz add-zsh-hook
 
 # Run after changing directory.
 _after_cd() {
-    ls -a
+    gls --color --group-directories-first -F  -A
 }
 
 add-zsh-hook chpwd _after_cd
