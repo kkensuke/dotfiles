de0152e556ec8e321b53f50d483b0e8522633bf8
kkensuke <47603428+kkensuke@users.noreply.github.com>
2026-09-10 00:56:35 +0900

    🗑️ REMOVE:

--- setup/4_lns.sh
+++ setup/4_lns.sh
@@ -8,13 +8,6 @@ export GITHUB="$HOME/github"
 export DOTFILES="$GITHUB/dotfiles"
 
 
-# Make symlinks for ignored files
-ln -sf /Users/kkensuke/Library/Mobile\ Documents/com~apple~CloudDocs/ignore $GITHUB/
-ln -sf $GITHUB/ignore/dotfiles_ignore/.gitconfig $GITHUB/dotfiles/git/.gitconfig
-ln -sf $GITHUB/ignore/dotfiles_ignore/zsh_ignore $GITHUB/dotfiles/zsh/
-ln -sf $GITHUB/ignore/espanso_ignore $GITHUB/espanso/
-
-
 # dotfiles
 cd $DOTFILES/home/
 for i in .[a-z]*
